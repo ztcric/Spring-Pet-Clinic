c73eb7b1e10028a252a5ebe4b5a14157e95c9682
Cliff <tzcliff@outlook.com>
2020-02-20 11:42:22 -0500

    List Owner using map

--- spring-pet-clinic/pet-clinic-web/src/main/java/com/cliff/springpetclinic/controllers/OwnerController.java
+++ spring-pet-clinic/pet-clinic-web/src/main/java/com/cliff/springpetclinic/controllers/OwnerController.java
@@ -1,12 +1,23 @@
 package com.cliff.springpetclinic.controllers;
 
+import com.cliff.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+
+    private final  OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index.html", "/owners/index"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
