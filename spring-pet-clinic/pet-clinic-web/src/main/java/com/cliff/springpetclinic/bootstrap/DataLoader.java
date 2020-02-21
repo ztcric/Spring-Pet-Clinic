bd906c185f82ba4d13ecc28175cb5cd0ab6caf2d
Cliff <tzcliff@outlook.com>
2020-02-21 15:18:53 -0500

    Internationalization Properties

--- spring-pet-clinic/pet-clinic-web/src/main/java/com/cliff/springpetclinic/bootstrap/DataLoader.java
+++ spring-pet-clinic/pet-clinic-web/src/main/java/com/cliff/springpetclinic/bootstrap/DataLoader.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
