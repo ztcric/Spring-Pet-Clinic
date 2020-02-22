daa458bd54bb652c8d270e53e525df2b111b350a
Cliff <tzcliff@outlook.com>
2020-02-22 15:58:38 -0500

    added PetType Service

---
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/cliff/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.cliff.springpetclinic.services;
+
+import com.cliff.springpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/cliff/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package com.cliff.springpetclinic.services.map;
+
+import com.cliff.springpetclinic.models.PetType;
+import com.cliff.springpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
